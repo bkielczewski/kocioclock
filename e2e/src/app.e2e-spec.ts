24748f94b6f0150ca5dd6bcef0be8ce89f0faa6e
Bartosz Kielczewski <bartosz@kielczewski.eu>
2019-07-27 00:46:05 +0100

    Removed unused method from AppPage, made the other one static

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,14 +2,9 @@ import { browser, logging } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
-  });
 
   it('should display main page', () => {
-    page.navigateTo();
+    AppPage.navigateTo();
   });
 
   afterEach(async () => {
