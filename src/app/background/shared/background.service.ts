22be0f4e948e960c64e5dfe84fd5d3e9c94cb4d1
Bartosz Kielczewski <b.kielczewski@jbs.cam.ac.uk>
2019-07-28 13:25:43 +0100

    Removed further videos because of Pi

--- src/app/background/shared/background.service.ts
+++ src/app/background/shared/background.service.ts
@@ -11,11 +11,9 @@ export class BackgroundService {
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2FBoat%20in%20Caramoan.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2FGubat%20Beach.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-clear-water-1559888911402.mp4', type: BackgroundType.VIDEO },
-    { url: 'https://storage.googleapis.com/coverr-main/mp4%2FBlue%20Joy.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-the-islands-1561045392058.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-ocean-and-footsteps-1559889043636.mp4', type: BackgroundType.VIDEO },
-    { url: 'https://storage.googleapis.com/coverr-main/mp4%2FTwo-Swimmers.mp4', type: BackgroundType.VIDEO },
-    { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-spider-boat-1561050987454.mp4', type: BackgroundType.VIDEO }
+    { url: 'https://storage.googleapis.com/coverr-main/mp4%2FTwo-Swimmers.mp4', type: BackgroundType.VIDEO }
   ];
 
   constructor() {
