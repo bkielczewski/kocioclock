db11b00553d13880e45cd766a3d58057b0f0f3f3
Bartosz Kielczewski <bartosz@kielczewski.eu>
2019-07-29 00:29:33 +0100

    Added fade in and fade out transitions between videos

--- src/app/background/background/background.component.ts
+++ src/app/background/background/background.component.ts
@@ -1,3 +1,4 @@
+import { animate, AnimationEvent, state, style, transition, trigger } from '@angular/animations';
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Background } from '../shared/background';
 import { BackgroundService } from '../shared/background.service';
@@ -5,10 +6,27 @@ import { BackgroundService } from '../shared/background.service';
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
-  styleUrls: ['./background.component.scss']
+  styleUrls: ['./background.component.scss'],
+  animations: [
+    trigger('playStop', [
+      state('play', style({
+        opacity: 1
+      })),
+      state('stop', style({
+        opacity: 0
+      })),
+      transition('play => stop', [
+        animate('1s')
+      ]),
+      transition('stop => play', [
+        animate('1s')
+      ])
+    ])
+  ]
 })
 export class BackgroundComponent implements OnInit, AfterViewInit {
 
+  isPlaying = false;
   @ViewChild('videoElement', { read: false, static: false }) videoElement: ElementRef<HTMLVideoElement>;
 
   constructor(private backgroundService: BackgroundService) {
@@ -22,7 +40,13 @@ export class BackgroundComponent implements OnInit, AfterViewInit {
   }
 
   onVideoEnded() {
-    this.playNext();
+    this.isPlaying = false;
+  }
+
+  onAnimationDone($event: AnimationEvent) {
+    if ($event.toState === 'stop' && this.videoElement) {
+      this.playNext();
+    }
   }
 
   private playNext() {
@@ -35,7 +59,9 @@ export class BackgroundComponent implements OnInit, AfterViewInit {
     this.videoElement.nativeElement.src = background.url;
     this.videoElement.nativeElement.muted = true;
     this.videoElement.nativeElement.load();
-    this.videoElement.nativeElement.play().catch((e: DOMException) => console.log(e.message));
+    this.videoElement.nativeElement.play()
+      .then(() => this.isPlaying = true)
+      .catch((e: DOMException) => console.log(e.message));
   }
 
 }
