106145325d58faffc7af1dba582c58d3ce34a0bf
Bartosz Kielczewski <b.kielczewski@jbs.cam.ac.uk>
2019-07-28 12:51:00 +0100

    Removed two videos as Pi was struggling

--- src/app/background/shared/background.service.ts
+++ src/app/background/shared/background.service.ts
@@ -12,12 +12,10 @@ export class BackgroundService {
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2FGubat%20Beach.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-clear-water-1559888911402.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2FBlue%20Joy.mp4', type: BackgroundType.VIDEO },
-    { url: 'https://storage.googleapis.com/coverr-main/mp4%2FFerry_Tour.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-the-islands-1561045392058.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-ocean-and-footsteps-1559889043636.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2FTwo-Swimmers.mp4', type: BackgroundType.VIDEO },
-    { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-spider-boat-1561050987454.mp4', type: BackgroundType.VIDEO },
-    { url: 'https://storage.googleapis.com/coverr-main/mp4%2FGolden-wings.mp4', type: BackgroundType.VIDEO }
+    { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-spider-boat-1561050987454.mp4', type: BackgroundType.VIDEO }
   ];
 
   constructor() {
