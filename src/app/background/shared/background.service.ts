765397e96a158dd66b15e7c970d0ad8f8a9fd39f
Bartosz Kielczewski <b.kielczewski@jbs.cam.ac.uk>
2019-07-28 14:00:12 +0100

    Trying with 3 videos as it's either Pi or some memory leak

--- src/app/background/shared/background.service.ts
+++ src/app/background/shared/background.service.ts
@@ -8,9 +8,6 @@ export class BackgroundService {
 
   private current = 0;
   private backgrounds: Background[] = [
-    { url: 'https://storage.googleapis.com/coverr-main/mp4%2FBoat%20in%20Caramoan.mp4', type: BackgroundType.VIDEO },
-    { url: 'https://storage.googleapis.com/coverr-main/mp4%2FGubat%20Beach.mp4', type: BackgroundType.VIDEO },
-    { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-clear-water-1559888911402.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-the-islands-1561045392058.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-ocean-and-footsteps-1559889043636.mp4', type: BackgroundType.VIDEO },
     { url: 'https://storage.googleapis.com/coverr-main/mp4%2FTwo-Swimmers.mp4', type: BackgroundType.VIDEO }
